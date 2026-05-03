7a33e1cfb88caca84361f015b83b85c438cb7728
Rohan Waseem <rohanwaseem985@gmail.com>
2026-05-03 17:27:18 +0500

    Api correct

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         const handlelogin =async(e)=>{
             e.preventDefault()
             try {
-              const res = await fetch("`${API}/login`",{
+              const res = await fetch(`${API}/login`,{
                 method:"POST",
                 headers:{"Content-Type":"application/json"},
                 body:JSON.stringify({phone , password})
